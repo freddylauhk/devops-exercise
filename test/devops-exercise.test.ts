8fa330a27fc6f5e55928bac5e8e007e3e870c284
freddylauhk <freddy.kmlau@gmail.com>
2024-05-23 03:08:17 -0400

    Initial commit

---
+++ test/devops-exercise.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DevopsExercise from '../lib/devops-exercise-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/devops-exercise-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DevopsExercise.DevopsExerciseStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
